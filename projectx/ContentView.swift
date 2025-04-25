9d4ff5f03b548b25fc3c6ed863d20f6259fc35b0
LukmanAfsal <lukman_bv2019@ug.cusat.ac.in>
2025-04-25 16:35:33 +0530

    Initial Commit

---
+++ projectx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projectx
+//
+//  Created by jeboy on 25/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projectx/projectxApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectxApp.swift
+//  projectx
+//
+//  Created by jeboy on 25/04/25.
+//
+
+import SwiftUI
+
+@main
+struct projectxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
