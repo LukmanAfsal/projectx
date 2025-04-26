e90389f451946fd7a206fecce9f073c4dbc6b0f8
LukmanAfsal <lukman_bv2019@ug.cusat.ac.in>
2025-04-26 17:40:25 +0530

    Updated

--- projectx/CloudinaryConfig.swift
+++ projectx/CloudinaryConfig.swift
@@ -8,10 +8,8 @@
 import Foundation
 
 struct CloudinaryConfig {
-    static let cloudName = "dl4qvqvjv" // Replace with your cloud name
-    static let apiKey = "391139438517655" // Replace with your API key
+    static let cloudName = "dl4qvqvjv"
+    static let apiKey = "391139438517655"
     static let apiSecret = "huav7nNKm5cCCZRbranm1CqSu2M"
     static let uploadPreset = "projectx_uploads"
-    
-    //
 }
